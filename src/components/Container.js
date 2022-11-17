86b50d0cbf22661d73b05245301cda70f973200b
Abdulbari <abdulbari.112917@gmail.com>
2022-11-17 22:52:23 +0300

    Completed Assignment

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,10 +8,12 @@ function Container({
   contentPosition = "center", // possible values: left, right, center
 }) {
   return (
-    <div className={`container ${textPosition}`}>
+    <div className={`container ${contentPosition} ${textPosition}`}>
       {header ? <h2>{header}</h2> : null}
 
-      <div className={`container-children ${direction} ${contentPosition}`}>
+      <div
+        className={`container-children ${direction} ${direction} ${contentPosition}`}
+      >
         {children}
       </div>
     </div>
